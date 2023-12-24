dafa9a084209c111e946b679ec4bb520b7b91518
linhtran <lt@gmail.com>
2023-12-24 12:02:29 +0700

    Restructure project foler

---
+++ src/ApiGateway/FT.Travelako.OcelotApiGw/Installer/SystemInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Provider.Polly;
+using Ocelot.Provider.Consul;
+
+
+namespace FT.Travelako.OcelotApiGw.Installer
+{
+    public class SystemInstaller : IInstaller
+    {
+        public void InstallerServicesInAssembly(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Microservices demo", Version = "v1" });
+            });
+
+            services.AddEndpointsApiExplorer();
+        }
+    }
+}
