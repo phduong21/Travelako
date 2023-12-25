def033d249a37a6b376acd8628ff0e8772e180b3
phduong21 <phduong.bk@gmail.com>
2023-12-26 00:20:52 +0700

    booking base service

---
+++ src/Services/FT.Travelako.Services.BookingAPI/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FT.Travelako.Services.BookingAPI.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ src/Services/FT.Travelako.Services.BookingAPI/Models/DTOs/GetBookingRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿using FT.Travelako.Common.BaseInterface;
+
+namespace FT.Travelako.Services.BookingAPI.Models.DTOs
+{
+    public class GetBookingRequestDTO : IBaseRequestModel
+    {
+        public string? Code { get; set; }
+    }
+}
