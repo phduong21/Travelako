218e756b29cdea5dccbda28ae7869495857d133e
phduong21 <phduong.bk@gmail.com>
2023-12-25 11:05:06 +0700

    helpers common

---
+++ src/Common/FT.Travelako.Common/Helpers/GuidHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT.Travelako.Common.Helpers
+{
+    public static class GuidHelper
+    {
+        public static Guid NewGuid(string guid = null)
+        {
+            return string.IsNullOrWhiteSpace(guid) ? Guid.NewGuid() : new Guid(guid);
+        }
+
+        public static string ToShortString(this Guid id)
+        {
+            return Convert.ToBase64String(id.ToByteArray())
+                    .Replace('+', '-')
+                    .Replace('/', '_')
+                    .Replace("=", "");
+        }
+    }
+}
