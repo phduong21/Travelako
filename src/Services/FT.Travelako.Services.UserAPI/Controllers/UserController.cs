e337883808b9310f6cdfa4e9f5b3bb331bd7b96a
phduong21 <phduong.bk@gmail.com>
2023-12-26 00:56:24 +0700

    user base service

---
+++ src/Services/FT.Travelako.Services.UserAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using FT.Travelako.Common.BaseModels;
+using FT.Travelako.Common.Controller;
+using FT.Travelako.Services.UserAPI.Models.DTOs;
+using FT.Travelako.Services.UserAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FT.Travelako.Services.TravelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ApiBaseController
+    {
+        public UserController(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+         
+        }
+
+        [HttpGet]
+        public async Task<GenericAPIResponse> GetAllTravel([FromRoute] GetUserRequestDTO? model)
+        {
+            return await ExecutionService<GetUserRequestDTO, GetUserService>(model);
+        }
+
+
+        [HttpGet("getAllUser")]
+        public async Task<GenericAPIResponse> GetTravel([FromRoute] GetUserRequestDTO? model)
+        {
+            return await ExecutionService<GetUserRequestDTO, GetUserService>(model);
+        }
+    }
+}
---
+++ src/Services/FT.Travelako.Services.UserAPI/Services/GetUserService.cs
@@ -0,0 +1,23 @@
+﻿using FT.Travelako.Common.BaseImplementation;
+using FT.Travelako.Common.BaseModels;
+using FT.Travelako.Services.UserAPI.Models.DTOs;
+
+namespace FT.Travelako.Services.UserAPI.Services
+{
+    public class GetUserService : BaseExecutionService<GetUserRequestDTO>
+    {
+        public GetUserService() { }
+
+        public override async Task<GenericAPIResponse> ExecuteApi(GetUserRequestDTO model)
+        {
+            //logic here here
+            //call to repo here
+            return  new GenericAPIResponse
+            {
+                IsSuccess = true,
+                Message = "123456789a-user",
+            };
+        }
+
+    }
+}
