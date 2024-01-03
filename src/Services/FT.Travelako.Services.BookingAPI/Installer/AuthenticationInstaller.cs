e35ade01455c3fde7ad1a4e2bfe6518846d0e474
tungdp1 <duongphitung12a5@gmail.com>
2024-01-04 00:43:44 +0700

    Implement authen for all service

---
+++ src/Services/FT.Travelako.Services.BookingAPI/Installer/AuthenticationInstaller.cs
@@ -0,0 +1,41 @@
+﻿using FT.Travelako.Common.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FT.Travelako.Services.BookingAPI.Installer
+{
+    public class AuthenticationInstaller : IInstaller
+    {
+        public void InstallerServicesInAssembly(IServiceCollection service, IConfiguration configuration)
+        {
+
+
+            var authenConfiguration = new AppSettingsConfiguration();
+            configuration.GetSection("AppSettings").Bind(authenConfiguration);
+            service.AddSingleton<IAppSettingsConfiguration, AppSettingsConfiguration>(x => authenConfiguration);
+            service.AddAuthentication(
+                opt =>
+                {
+                    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+             .AddJwtBearer(opt =>
+             {
+                 opt.RequireHttpsMetadata = false;
+                 opt.SaveToken = true;
+                 opt.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     // auto gen token
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     // sign to token
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenConfiguration.SecretKey)),
+                     ValidateLifetime = true,
+                     ClockSkew = TimeSpan.Zero
+                 };
+             });
+        }
+    }
+}
--- src/Services/FT.Travelako.Services.UserAPI/Program.cs
+++ src/Services/FT.Travelako.Services.UserAPI/Program.cs
@@ -2,8 +2,11 @@ using AutoMapper;
 using FT.Travelako.Services.UserAPI;
 using FT.Travelako.Services.UserAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using FT.Travelako.Service.Core.ServiceDiscovery;
+using FT.Travelako.Services.UserAPI.Installer;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddConsul(builder.Configuration.GetServiceConfig());
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<AppDbContext>(opts =>
 {
@@ -20,7 +23,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Configuration.AddJsonFile("appsettings.json", false, true)
+        .AddEnvironmentVariables();
+ConfigurationManager configuration = builder.Configuration;
+builder.Services.InstallerServicesInAssembly(configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
