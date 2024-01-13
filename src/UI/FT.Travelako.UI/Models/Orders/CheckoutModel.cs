de22f1a2f21b631481d8526fac20cea62f9cb058
linhtran <lt@gmail.com>
2024-01-14 01:48:44 +0700

    Integrate Booking Api

---
+++ src/UI/FT.Travelako.UI/Models/Orders/CheckoutModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FT.Travelako.UI.Models.Orders
+{
+    public class CheckoutModel
+    {
+        public decimal TotalCost { get; set; }
+        public int Status { get; set; }
+        public Guid TravelId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/UI/FT.Travelako.UI/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using FT.Travelako.UI.Extensions;
+using FT.Travelako.UI.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FT.Travelako.UI.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersById(string userId)
+        {
+            var response = await _client.GetAsync($"/booking/v1/Order/get-orders/{userId}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+
+        public async Task CheckoutOrder(CheckoutModel checkoutModel)
+        {
+            var response = await _client.PostAsJsonAsync($"/booking/v1/Order/check-out", checkoutModel);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+
+        public async Task DeleteOrder(string userId)
+        {
+            var response = await _client.DeleteAsync($"/booking/v1/Order/{userId}");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+    }
+}
