75bfc7ddbda3e7fdc96ff2e5906c24fe92c4e65b
linhtran <lt@gmail.com>
2024-01-14 13:48:22 +0700

    Update Order Service from UI

--- src/UI/FT.Travelako.UI/Services/OrderService.cs
+++ src/UI/FT.Travelako.UI/Services/OrderService.cs
@@ -1,5 +1,7 @@
 ﻿using FT.Travelako.UI.Extensions;
+using FT.Travelako.UI.Infrastructure.API;
 using FT.Travelako.UI.Models.Orders;
+using NuGet.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -10,21 +12,25 @@ namespace FT.Travelako.UI.Services
     public class OrderService : IOrderService
     {
         private readonly HttpClient _client;
+        private readonly string _remoteServiceBaseUrl;
 
         public OrderService(HttpClient client)
         {
+            _remoteServiceBaseUrl = $"/booking/v1/Order";
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
         public async Task<IEnumerable<OrderResponseModel>> GetOrdersById(string userId)
         {
-            var response = await _client.GetAsync($"/booking/v1/Order/get-orders/{userId}");
+            var getOrderByUserIdUri = ApiOrder.GetOrderByUserId(_remoteServiceBaseUrl, userId);
+            var response = await _client.GetAsync(getOrderByUserIdUri);
             return await response.ReadContentAs<List<OrderResponseModel>>();
         }
 
         public async Task CheckoutOrder(CheckoutModel checkoutModel)
         {
-            var response = await _client.PostAsJsonAsync($"/booking/v1/Order/check-out", checkoutModel);
+            var checkoutOrderUri = ApiOrder.CheckOutOrder(_remoteServiceBaseUrl);
+            var response = await _client.PostAsJsonAsync(checkoutOrderUri, checkoutModel);
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception("Something went wrong when calling api.");
@@ -33,7 +39,8 @@ namespace FT.Travelako.UI.Services
 
         public async Task DeleteOrder(string userId)
         {
-            var response = await _client.DeleteAsync($"/booking/v1/Order/{userId}");
+            var deleteOrderUri = ApiOrder.DeleteOrder(_remoteServiceBaseUrl, userId);
+            var response = await _client.DeleteAsync(deleteOrderUri);
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception("Something went wrong when calling api.");
