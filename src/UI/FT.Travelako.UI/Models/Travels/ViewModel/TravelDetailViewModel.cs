3f7940f2a7f1978619d3d5172da2befb33787ec5
Lupin99 <kaito1999gl@gmail.com>
2024-01-18 23:38:03 +0700

    update code

--- src/UI/FT.Travelako.UI/Models/Travels/ViewModel/TravelDetailViewModel.cs
+++ src/UI/FT.Travelako.UI/Models/Travels/ViewModel/TravelDetailViewModel.cs
@@ -4,6 +4,12 @@ namespace FT.Travelako.UI.Models.Travels.ViewModel
 {
     public class TravelDetailViewModel
     {
+        public TravelDetailViewModel()
+        {
+            TravelDetail = new TravelDetailModel();
+            RecentTravels = new TravelListingResponseModel();
+            Author = new UserDetailResponseModelNew();
+        }
         public TravelDetailModel TravelDetail { get; set; }
         public TravelListingResponseModel RecentTravels { get; set; }
         public UserDetailResponseModelNew Author { get; set; }
