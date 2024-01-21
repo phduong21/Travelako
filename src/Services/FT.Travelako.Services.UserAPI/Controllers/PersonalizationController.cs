c9db45d16e08f0596fb62b4e4b310159fa1c4230
phduong21 <phduong.bk@gmail.com>
2024-01-21 20:35:33 +0700

    update ui create user

--- src/Services/FT.Travelako.Services.UserAPI/Controllers/PersonalizationController.cs
+++ src/Services/FT.Travelako.Services.UserAPI/Controllers/PersonalizationController.cs
@@ -18,16 +18,16 @@ namespace FT.Travelako.Services.UserAPI.Controllers
         }
 
         [HttpPut]
-        //[AuthorizeFTFilter]
-        //[Authorize(Roles = "user,business,administrator")]
+        [AuthorizeFTFilter]
+        [Authorize(Roles = "user,business,administrator")]
         public async Task<GenericAPIResponse> UpdateUserPersonalize(PersonalizeRequest model)
         {
             return await ExecutionService<PersonalizeRequest, UpdateUserPersonalizeService>(model);
         }
 
         [HttpGet]
-        //[AuthorizeFTFilter]
-        //[Authorize(Roles = "user,business,administrator")]
+        [AuthorizeFTFilter]
+        [Authorize(Roles = "user,business,administrator")]
         public async Task<GenericAPIResponse> GetUserPersonalize([FromQuery] GetPersonalizeRequest model)
         {
             return await ExecutionService<GetPersonalizeRequest, GetPersonalizeUserService>(model);
--- src/UI/FT.Travelako.UI/Models/Users/ViewModel/SignUpVM.cs
+++ src/UI/FT.Travelako.UI/Models/Users/ViewModel/SignUpVM.cs
@@ -7,24 +7,30 @@ namespace FT.Travelako.UI.Models.Users.ViewModel
     public class SignUpVM
     {
         [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "User Name *")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email *")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [Display(Name = "Password *")]
         public string Password { get; set; }
 
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password *")]
         public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name *")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "Last Name *")]
         public string LastName { get; set; }
 
         public string? Address { get; set; }
