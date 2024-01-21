e9263e90bd3c5871d74a801dcf639b6de22e37c0
Lupin99 <kaito1999gl@gmail.com>
2024-01-22 00:57:57 +0700

    update fe logic code

--- src/UI/FT.Travelako.UI/wwwroot/js/pagination.js
+++ src/UI/FT.Travelako.UI/wwwroot/js/pagination.js
@@ -1,16 +1,37 @@
 let loadMoreBtn = document.querySelector('#load-more-btn');
+let boxesList = [...document.querySelectorAll('.travel-listing .travel-detail-card')];
 let currentItem = 3;
+if (boxesList.length <= currentItem) {
+    loadMoreBtn.style.display = "none";
+}
 
 loadMoreBtn.onclick = () => {
-    let boxes = [...document.querySelectorAll('.travel-listing .travel-detail-card')];
-    console.log("alo")
     for (var i = currentItem; i < currentItem + 3; i++) {
-        boxes[i].style.display = "block";
+        boxesList[i].style.display = "block";
 
         currentItem += 3;
 
-        if (currentItem >= boxes.length) {
+        if (currentItem >= boxesList.length) {
             loadMoreBtn.style.display = "none";
+        }
+    }
+}
+
+let loadMoreBtnSuggest = document.querySelector('#load-more-btn-suggest');
+let boxesSuggest = [...document.querySelectorAll('.travel-list-suggest .travel-detail-card')];
+let currentItemSuggest = 3;
+if (boxesSuggest.length <= currentItemSuggest) {
+    loadMoreBtnSuggest.style.display = "none";
+}
+
+loadMoreBtnSuggest.onclick = () => {
+    for (var i = currentItemSuggest; i < currentItemSuggest + 3; i++) {
+        boxesSuggest[i].style.display = "block";
+
+        currentItemSuggest += 3;
+
+        if (currentItemSuggest >= boxesSuggest.length) {
+            loadMoreBtnSuggest.style.display = "none";
         }
     }
 }
